383f91da1b2ad8214ec2c8fc97517970bca76a91
DevNitinYewale <97602200+DevNitinYewale@users.noreply.github.com>
2022-01-18 16:46:53 +0530

    Commit

--- Demo/src/FirstPackage/Display.java
+++ Demo/src/FirstPackage/Display.java
@@ -3,5 +3,7 @@ package FirstPackage;
 public class Display {
 	public static void main(String[] args) {
 		System.out.println("haii i am println");
+		
+		System.out.println("My Name is Nitin");
 	}
 }
